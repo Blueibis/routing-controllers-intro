0fa3e57e9054701230f6968f0ebf6690439e6d95
Blueibis <jennie.li.2011@gmail.com>
2017-06-08 14:34:46 -0400

    added kitten route-view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,14 @@
 class PagesController < ApplicationController
   def welcome
+    @header = "This is the welcome page"
     render :about
   end
   def about
   end
   def contest
+  end
+  def kitten
+    size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{size}/#{size}/cats"
   end
 end
