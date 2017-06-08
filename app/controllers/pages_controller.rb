6163ca72dd5309a62e1fb3b16764851e97ada2b7
Blueibis <jennie.li.2011@gmail.com>
2017-06-08 14:45:22 -0400

    add kittens

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
   def welcome
     @header = "This is the welcome page"
     render :about
@@ -8,6 +9,12 @@ class PagesController < ApplicationController
   def contest
   end
   def kitten
+
+  end
+  def kittens
+
+  end
+  def set_kitten_url
     size = params[:size]
     @kitten_url = "http://lorempixel.com/#{size}/#{size}/cats"
   end
