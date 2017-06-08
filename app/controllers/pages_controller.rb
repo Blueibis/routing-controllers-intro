9fa03744c0150629aa1e3036bd1755d0a47a2992
Blueibis <jennie.li.2011@gmail.com>
2017-06-08 15:21:17 -0400

    Add secrets page /w key

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,20 +4,32 @@ class PagesController < ApplicationController
     @header = "This is the welcome page"
     render :about
   end
+
   def about
   end
+
   def contest
     flash[:notice] = "Sorry, the contest has ended."
     redirect_to '/welcome'
   end
+
   def kitten
-
   end
+
   def kittens
-
   end
+
   def set_kitten_url
     size = params[:size]
     @kitten_url = "http://lorempixel.com/#{size}/#{size}/cats"
   end
+
+  def secrets
+    if params[:magic_word] != "kittens"
+      flash[:alert] = "Incorrect password"
+      redirect_to '/welcome'
+    else
+    end
+  end
+
 end
